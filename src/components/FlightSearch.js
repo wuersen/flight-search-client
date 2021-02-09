5908dc458592c193033e70b62c1c509e9cffb611
wuersen <wuersen@hotmail.com>
2021-02-09 22:29:27 +1100

    tue2229

--- src/components/FlightSearch.js
+++ src/components/FlightSearch.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import _ from 'underscore'
 
 import SearchForm from './SearchForm'
 import SearchResult from './SearchResult'
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,9 +1,44 @@
 import React, {Component} from 'react';
 
 class SearchForm extends Component {
+  constructor () {
+    super();
+    this.state = {
+      date: '',
+      origin: '',
+      destination: ''
+    }
+    this._dateInput = this._dateInput.bind(this);
+    this._originInput = this._originInput.bind(this);
+    this._destinationInput = this._destinationInput.bind(this);
+
+  }
+
+_dateInput(event) {
+  this.setState({
+    date: event.target.value
+  });
+}
+_originInput(event) {
+  this.setState({
+    origin: event.target.value
+  });
+}
+_destinationInput(event) {
+  this.setState({
+    destination: event.target.value
+  });
+}
+
+
    render() {
      return (
-       <h2>Search form</h2>
+       <form onSubmit={this._handleSubmit}>
+          <input type="text" placeholder="date" required onInput={ this._dateInput }/>
+          <input type="text" placeholder="origin" required onInput={ this._originInput }/>
+          <input type="text" placeholder="destination" required onInput={ this._destinationInput }/>
+          <button type="submit">Search flight</button>
+      </form>
      )
    }
 }
